70ec7296363d80b03cabd85d44451743c1717cf8
Ian Walter <public@iankwalter.com>
2018-08-19 21:13:25 -0400

    Adding sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Ian Walter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        precision: 8
+      }
+    }
+  ]
 }
