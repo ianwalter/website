b69fae9a03372574109d985515b1a2516e54de3b
Ian Walter <public@iankwalter.com>
2018-09-08 17:00:02 -0400

    Adding about content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import avatar from '../assets/ian.jpg'
 
 export default () => (
-  <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+  <div className="container p-4">
+
+    <h1>Hi, my name is Ian Kennington Walter</h1>
+
+    <p className="content mt-5">
+
+      <img
+        src={avatar}
+        className="avatar float-sm-left rounded mr-5 mt-4 mb-5 mb-sm-4 mb-xl-0" />
+
+      I'm a professional software developer who loves creating
+      things. I mostly work on web applications and I'm currently helping
+      {' '}<a href="https://getbinx.com" className="fontWeight400">Binx Health</a>{' '}
+      create an application to make it easy for people to get accurate at-home
+      testing for sextually-transmitted infections.
+
+      I like working on side projects and contributing to open source software
+      as much as I can.
+      {' '}<a href="" className="fontWeight400">Click here</a> to take a look at some of the projects I'm
+      working on.
+
+    </p>
+
+    <p className="content">
+      This is my personal website and it's built using
+      {' '}<a href="" className="fontWeight400">GatsbyJS</a> and
+      {' '}<a href="" className="fontWeight400">Bootstrap</a>. It's hosted on
+      {' '}<a href="" className="fontWeight400">Netlify</a> and you can find the source code on
+      {' '}<a href="" className="fontWeight400">GitHub</a>.
+    </p>
+
   </div>
 )
